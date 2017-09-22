9ffa3789434c1af2c09bd9d6328de9dd53831a03
usuario <usuario@10.22.148.62>
2017-09-22 13:46:17 +0100

    Kata 1.2  calcular edad con calendario gregoriano

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,11 +1,15 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+
+import java.util.GregorianCalendar;
 
 public class Kata1 {
     public static void main(String[] args) {
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(1998, 8, 19);
         Person person;
-        person = new Person("El Bolitas", new Date(98,8,19));
+        person = new Person("El Bolitas", date );
         System.out.println(person.getName() + " tiene " + person.getAge() + " anos");
     }
     
