db809e6b8737ccf51d2b927f446ec32fdb9409d5
usuario <usuario@10.22.148.62>
2017-09-22 13:24:38 +0100

    Kata 1.1 si quieres saber la edad del bolitas, este es tu lugar

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,12 @@
+package kata1;
+
+import java.util.Date;
+
+public class Kata1 {
+    public static void main(String[] args) {
+        Person person;
+        person = new Person("El Bolitas", new Date(98,8,19));
+        System.out.println(person.getName() + " tiene " + person.getAge() + " años");
+    }
+    
+}
