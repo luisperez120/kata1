56a9e78adb27dac36ef8e23b2d709cc578bbca65
usuario <usuario@10.22.148.62>
2017-09-22 13:26:52 +0100

    Kata 1.1 calcular edad

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -6,7 +6,7 @@ public class Kata1 {
     public static void main(String[] args) {
         Person person;
         person = new Person("El Bolitas", new Date(98,8,19));
-        System.out.println(person.getName() + " tiene " + person.getAge() + " años");
+        System.out.println(person.getName() + " tiene " + person.getAge() + " anos");
     }
     
 }
